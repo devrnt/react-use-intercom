11eb1624a0f3c616621d163e8283684e5471209c
devrnt <devrient.jonas@gmail.com>
2020-05-18 21:00:23 +0200

    test: add provider with event callbacks

--- cypress/integration/provider.ts
+++ cypress/integration/provider.ts
@@ -1,9 +1,62 @@
 /// <reference types="cypress" />
 
-context('provider', () => {
+describe('provider', () => {
   it('should render children', () => {
     cy.visit('/provider');
 
     cy.get('p').should('be.visible');
+  });
+});
+
+describe('provider with events', () => {
+  it('should render children', () => {
+    cy.visit('/providerEvents');
+
+    cy.get('p').should('be.visible');
+  });
+
+  it('should render default text', () => {
+    cy.visit('/providerEvents');
+
+    cy.contains('default');
+  });
+
+  it('should not execute `onShow` when not booted', () => {
+    cy.visit('/providerEvents');
+
+    cy.get('[data-cy=onShowText]').should('have.text', 'default');
+    cy.get('[data-cy=show]').click();
+
+    cy.get('[data-cy=onShowText]').should('have.text', 'default');
+  });
+
+  it('should execute `onShow` event when clicking `show`', () => {
+    cy.visit('/providerEvents');
+
+    cy.get('[data-cy=onShowText]').should('have.text', 'default');
+    cy.get('[data-cy=boot]').click();
+    cy.get('[data-cy=show]').click();
+
+    cy.get('[data-cy=onShowText]').should('have.text', 'show was called');
+  });
+
+  it('should not execute `onHide` event when clicking `hide`', () => {
+    cy.visit('/providerEvents');
+
+    cy.get('[data-cy=onHideText]').should('have.text', 'default');
+    cy.get('[data-cy=boot]').click();
+
+    cy.get('[data-cy=onHideText]').should('have.text', 'default');
+  });
+
+  it('should execute `onHide` event when clicking `hide`', () => {
+    cy.visit('/providerEvents');
+
+    cy.get('[data-cy=onHideText]').should('have.text', 'default');
+    cy.get('[data-cy=boot]').click();
+    cy.get('[data-cy=show]').click();
+    cy.get('[data-cy=hide]').click();
+
+    cy.get('[data-cy=onHideText]').should('have.text', 'hide was called');
   });
 });
