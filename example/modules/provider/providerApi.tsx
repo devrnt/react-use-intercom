c3ad2b244c4fcdf37b0c722ecc3f7da217561e81
devrnt <devrient.jonas@gmail.com>
2020-10-15 20:48:32 +0200

    test: add integration test for api base

---
+++ example/modules/provider/providerApi.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import { IntercomProvider } from '../../../.';
+
+const Provider = () => {
+  return (
+    <IntercomProvider
+      appId="jcabc7e3"
+      apiBase="https://jcabc7e3.intercom-messenger.com"
+      autoBoot
+    >
+      <p>Intercom children</p>
+    </IntercomProvider>
+  );
+};
+
+export default Provider;
