3d3e9b92ddf9d73c76b1fab6ff2ae3c1ff7195cf
devrnt <devrient.jonas@gmail.com>
2020-12-07 09:39:10 +0100

    chore: add nextjs example

---
+++ examples/nextjs/pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import { IntercomProvider } from 'react-use-intercom';
+
+const INTERCOM_APP_ID = 'jcabc7e3';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <IntercomProvider appId={INTERCOM_APP_ID}>
+      <Component {...pageProps} />
+    </IntercomProvider>
+  );
+}
+
+export default MyApp
