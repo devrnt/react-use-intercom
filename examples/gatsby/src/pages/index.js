38cd3b46ee927a9bd5c4e2a4132ecd7b867ab3cf
devrnt <devrient.jonas@gmail.com>
2020-12-08 21:25:48 +0100

    chore: update gatsby example

--- examples/gatsby/src/pages/index.js
+++ examples/gatsby/src/pages/index.js
@@ -2,28 +2,19 @@ import React from "react";
 import { useIntercom } from "react-use-intercom";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
 
 const IndexPage = () => {
   const { boot } = useIntercom();
 
   return (
-    <Layout>
-      <SEO title="Home" />
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
       <button onClick={() => boot()}>Boot</button>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div
-        style={{
-          maxWidth: `300px`,
-          marginBottom: `1.45rem`,
-        }}
-      >
-        <Image />
-      </div>
-    </Layout>
+    </div>
   );
 };
 
