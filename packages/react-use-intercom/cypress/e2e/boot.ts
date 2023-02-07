059028fcfdca20e91ed8fb4d25c58deb525b4b22
devrnt <devrient.jonas@gmail.com>
2023-02-07 21:39:30 +0100

    Fix boot test

--- packages/react-use-intercom/cypress/e2e/boot.ts
+++ packages/react-use-intercom/cypress/e2e/boot.ts
@@ -1,17 +1,26 @@
-/// <reference types="cypress" />
-
 describe('boot', () => {
+  beforeEach(() => {
+    cy.visit('/useIntercom');
+
+    cy.intercept('https://api-iam.intercom.io/messenger/web/ping').as(
+      'intercomPing',
+    );
+  });
+
   afterEach(() => {
     cy.get('[data-cy=shutdown]').click();
   });
 
   it('should boot when calling `boot`', () => {
-    cy.visit('/useIntercom');
-
     cy.window().should('not.have.property', 'intercomSettings');
 
     cy.get('[data-cy=boot]').click();
-    cy.contains('.intercom-lightweight-app-launcher-icon-open');
+
+    // Wait for the route aliased as 'intercomPing' to respond
+    // without changing or stubbing its response
+    cy.wait('@intercomPing');
+
+    cy.get('.intercom-lightweight-app-launcher-icon-open').should('exist');
     cy.window().should('have.property', 'Intercom');
     cy.window().should('have.deep.property', 'intercomSettings', {
       app_id: 'jcabc7e3',
@@ -19,12 +28,13 @@ describe('boot', () => {
   });
 
   it('should boot with seeded data when calling `boot`', () => {
-    cy.visit('/useIntercom');
-
     cy.window().should('not.have.property', 'intercomSettings');
 
     cy.get('[data-cy=boot-seeded]').click();
-    cy.contains('.intercom-lightweight-app-launcher-icon-open');
+
+    cy.wait('@intercomPing');
+
+    cy.get('.intercom-lightweight-app-launcher-icon-open').should('exist');
     cy.window().should('have.property', 'Intercom');
     cy.window().should('have.deep.property', 'intercomSettings', {
       app_id: 'jcabc7e3',
@@ -33,19 +43,12 @@ describe('boot', () => {
   });
 
   it('should disable all methods before calling `boot`', () => {
-    cy.visit('/useIntercom');
-
     cy.get('[data-cy=update]').click();
-    cy.get('.intercom-lightweight-app-launcher-icon-open').should(
-      'not.exist',
-    );
+    cy.get('.intercom-lightweight-app-launcher-icon-open').should('not.exist');
     cy.get('[data-cy=update-seeded]').click();
-    cy.get('.intercom-lightweight-app-launcher-icon-open').should(
-      'not.exist',
-    );
+
+    cy.get('.intercom-lightweight-app-launcher-icon-open').should('not.exist');
     cy.get('[data-cy=show]').click();
-    cy.get('.intercom-lightweight-app-launcher-icon-open').should(
-      'not.exist',
-    );
+    cy.get('.intercom-lightweight-app-launcher-icon-open').should('not.exist');
   });
 });
