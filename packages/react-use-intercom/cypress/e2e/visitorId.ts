594ea7c612bbd071a80573846ef9cadd4e4de0fb
devrnt <devrient.jonas@gmail.com>
2023-02-08 22:28:37 +0100

    Fix visitorId test

--- packages/react-use-intercom/cypress/e2e/visitorId.ts
+++ packages/react-use-intercom/cypress/e2e/visitorId.ts
@@ -1,11 +1,28 @@
 /// <reference types="cypress" />
 
+before(() => {
+  cy.visit('/useIntercom');
+
+  cy.intercept('https://api-iam.intercom.io/messenger/web/ping').as(
+    'intercomPing',
+  );
+  cy.intercept('https://api-iam.intercom.io/messenger/web/open').as(
+    'intercomOpen',
+  );
+  cy.intercept('https://api-iam.intercom.io/messenger/web/metrics').as(
+    'intercomMetrics',
+  );
+  cy.intercept('https://api-iam.intercom.io/messenger/web/home_cards').as(
+    'intercomHomeCards',
+  );
+});
+
 describe('getVisitorId', () => {
   it('should get visitor id when calling `getVisitorId`', () => {
     cy.visit('/useIntercom');
 
     cy.get('[data-cy=boot]').click();
-    cy.wait(1000);
+    cy.wait('@intercomPing');
 
     cy.get('[data-cy="visitorId"]').click();
 
