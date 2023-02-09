ebe8f2659e0495ebacfd1b00c96f2ce8e6511cbf
devrnt <devrient.jonas@gmail.com>
2023-02-10 00:07:56 +0100

    Hello github pages

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -3,12 +3,10 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: '/react-use-intercom/',
   preview: {
     port: 5173,
-  },
-  server: {
-    // https://vitejs.dev/guide/static-deploy.html#github-pages
-    base: '/react-use-intercom/',
   },
   plugins: [react()],
 });
