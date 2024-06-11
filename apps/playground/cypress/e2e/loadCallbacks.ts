e7954c5f5d6c35e724bffe5c168b1057da7a2c60
Marc Knaup <marc@knaup.io>
2024-06-12 05:35:08 +0700

    Prevent caching of Intercom Messenger script during onLoad Cypress test

--- apps/playground/cypress/e2e/loadCallbacks.ts
+++ apps/playground/cypress/e2e/loadCallbacks.ts
@@ -2,6 +2,12 @@
 
 describe('onLoad/onLoadFailed', () => {
   it('should call onLoad when successful', () => {
+    cy.intercept('https://widget.intercom.io/widget/jcabc7e3', (request) => {
+      request.continue((response) => {
+        response.headers['cache-control'] = 'no-cache';
+      });
+    });
+
     cy.visit('/useIntercomWithLoadCallbacks');
 
     cy.get('[data-cy=call]').should(($p) =>
@@ -10,11 +16,11 @@ describe('onLoad/onLoadFailed', () => {
   });
 
   it('should call onLoadFailed when not successful', () => {
-    cy.visit('/useIntercomWithLoadCallbacks');
-
     cy.intercept('https://widget.intercom.io/widget/jcabc7e3', {
       forceNetworkError: true,
     });
+
+    cy.visit('/useIntercomWithLoadCallbacks');
 
     cy.get('[data-cy=call]').should(($p) =>
       expect($p).to.have.text('onLoadFailed was called!'),
