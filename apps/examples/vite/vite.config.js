1b33f924e5dfc4af7d79c724a8eb4f25bfc74d16
devrnt <devrient.jonas@gmail.com>
2025-02-08 21:27:22 +0100

    Update port vite example

--- apps/examples/vite/vite.config.js
+++ apps/examples/vite/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 9300,
+  },
   plugins: [react()],
   clearScreen: false,
 });
