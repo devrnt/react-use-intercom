7ff5a4e28ae7b051e602e3aa3124d1927f471c93
Nick Hibberd <nhibberd@gmail.com>
2025-08-01 17:45:53 -0700

    fix: sort imports in authTokens test file

--- packages/react-use-intercom/test/authTokens.test.tsx
+++ packages/react-use-intercom/test/authTokens.test.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import { act, renderHook } from '@testing-library/react';
+import * as React from 'react';
 
 import { IntercomProvider, useIntercom } from '../src';
 
